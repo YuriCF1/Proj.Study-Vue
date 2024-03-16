9f47c88a4858eaa61d8151d924e79dcf8e5516ee
Yuri Cruz <yuricruzf@gmail.com>
2024-03-15 21:11:02 -0300

    Importing new component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <script lang="ts">
 import Banner from './components/Banner.vue';
+import ComponentePrincipal from './components/ComponentePrincipal.vue';
 
 export default {
   // components: { Banner: Banner }
-  components: { Banner }
+  components: { Banner, ComponentePrincipal }
 }
 </script>
 
 <template>
   <Banner />
+  <ComponentePrincipal />
 </template>
 
-<style scoped></style>
+
+<!-- PARA MAIS FEATURES DO CSS NO VUE: https://vuejs.org/api/sfc-css-features.html -->
+<style scoped>
+/* .titulo {
+  color: red;
+} */
+</style>
