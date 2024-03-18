2c0b2fc0a0412cc75190bd5c6402aab69ff474dd
Yuri Cruz <yuricruzf@gmail.com>
2024-03-17 23:21:00 -0300

    Creating card

---
+++ src/components/CardCategoria.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+import type ICategoria from '@/interfaces/ICategoria';
+import type { PropType } from 'vue';
+
+export default {
+    props: {
+        categoria: Object as PropType<ICategoria>
+        // categoria: { type:  Object as PropType<ICategoria>, required: true } //Caso o TS reclamasse que talvez categoria fosse indefinida
+    }
+}
+</script>
+<template lang="">
+    {{categoria.nome}}
+    {{categoria.imagem}}
+    </template>
+<style scoped></style>
