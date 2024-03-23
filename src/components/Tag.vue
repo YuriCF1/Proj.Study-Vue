6798220135faea16d747822eabcfc4f9fc8382d8
Yuri Cruz <yuricruzf@gmail.com>
2024-03-22 21:52:51 -0300

    Setting new class

--- src/components/Tag.vue
+++ src/components/Tag.vue
@@ -1,17 +1,21 @@
 <script lang="ts">
 export default {
     props: {
-        texto: { type: String, required: true }
+        texto: { type: String, required: true },
+        // ativa: { type: Boolean, default: false } //Não preicso colocar o default, pois por padrão já é falso
+        ativa: Boolean
     }
 }
 </script>
 
 <template lang="">
-    <span class="tag">{{texto}}</span>
+    <!-- <span class="tag" :class="{ativa}"> -->
+        <span :class="['tag', {ativa}]">
+        {{texto}}
+    </span>
 </template>
 
 <style scoped>
-
 .tag {
     display: inline-block;
     border-radius: 0.5rem;
@@ -19,6 +23,12 @@ export default {
     padding: 0.5rem;
     text-align: center;
     transition: 0.2s;
+    color: var(--cinza);
+    background-color: var(--cinza-claro);
+    font-weight: 400;
+}
+
+.tag.ativa {
     color: var(--creme, #FFFAF3);
     background: var(--coral, #F0633C);
     font-weight: 700;
