15857568aad53283183b511a57503be45ab9a646
Yuri Cruz <yuricruzf@gmail.com>
2024-03-25 23:32:51 -0300

    Creaeting component to select items

---
+++ src/components/IngredienteSelecionavel.vue
@@ -0,0 +1,34 @@
+<script lang="ts">
+import Tag from "./Tag.vue"
+
+export default {
+    props: {
+        ingrediente: { type: String, required: true }
+    },
+    components: { Tag },
+    //Criando um estado = data()
+    data() {
+        return {
+            selecionado: false
+        }
+
+    }
+}
+</script>
+
+<template lang="">
+    <!-- v-on:click="selecionado = !selecionado" -->
+    <button
+    class="ingrediente"
+    @click="selecionado = !selecionado"
+    :aria-pressed="selecionado"
+    >
+        <Tag :texto="ingrediente" :ativa="selecionado"/>
+    </button>
+</template>
+
+<style scoped>
+.ingrediente {
+    cursor: pointer;
+}
+</style>
