04da3d5ffc2cbf48ca4bb7ac067bfd2efae2a8de
Yuri Cruz <yuricruzf@gmail.com>
2024-03-28 19:40:02 -0300

    Importing footer

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <script lang="ts">
 import Banner from './components/Banner.vue';
 import ComponentePrincipal from './components/ComponentePrincipal.vue';
+import Rodape from './components/Rodape.vue';
 
 export default {
   // components: { Banner: Banner }
-  components: { Banner, ComponentePrincipal }
+  components: { Banner, ComponentePrincipal, Rodape }
 }
 </script>
 
 <template>
   <Banner />
   <ComponentePrincipal />
+  <Rodape />
 </template>
 
 
