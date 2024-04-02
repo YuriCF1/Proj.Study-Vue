4ed640a801189b7e602ffd79a6487b5e113fd83c
Yuri Cruz <yuricruzf@gmail.com>
2024-04-02 19:02:56 -0300

    Refactoring function

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,9 +1,13 @@
 import type ICategoria from "@/interfaces/ICategoria";
+import type { IReceita } from "@/interfaces/IReceita";
 
 export async function obterCategorias() {
     const resposta = await fetch('https://gist.githubusercontent.com/antonio-evaldo/002ad55e1cf01ef3fc6ee4feb9152964/raw/bf463b47860043da3b3604ca60cffc3ad1ba9865/categorias.json');
 
     const categoriasGerais: ICategoria[] = await resposta.json()
+
+    console.log('Categorias HTTP', categoriasGerais);
+    console.log('Categorias HTTP', typeof(categoriasGerais));
 
     return categoriasGerais;
 
@@ -21,3 +25,12 @@ export async function obterCategorias() {
     // ]
 
 }
+
+export async function obterReceitas() {
+    return obterDadosURL<IReceita[]>('https://gist.githubusercontent.com/antonio-evaldo/002ad55e1cf01ef3fc6ee4feb9152964/raw/bf463b47860043da3b3604ca60cffc3ad1ba9865/receitas.json');
+  }
+
+  async function obterDadosURL<T>(url: string) {
+    const resposta = await fetch(url);
+    return resposta.json() as T;
+  }
