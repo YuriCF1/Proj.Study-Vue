9239c6b18844f228539cd898d40aedba67f7bf6f
Yuri Cruz <yuricruzf@gmail.com>
2024-04-07 17:47:05 -0300

    Fixing link

--- src/http/index.ts
+++ src/http/index.ts
@@ -27,16 +27,7 @@ export async function obterCategorias() {
 }
 
 export async function obterReceitas() {
-  // return obterDadosURL<IReceita[]>('https://gist.githubusercontent.com/antonio-evaldo/002ad55e1cf01ef3fc6ee4feb9152964/raw/bf463b47860043da3b3604ca60cffc3ad1ba9865/receitas.json');
-  const resposta = await fetch('https://gist.githubusercontent.com/antonio-evaldo/002ad55e1cf01ef3fc6ee4feb9152964/raw/bf463b47860043da3b3604ca60cffc3ad1ba9865/receitas.json');
-
-  const repostasObtidas: IReceita[] = await resposta.json()
-
-  console.log('Categorias HTTP', repostasObtidas);
-  console.log('Categorias HTTP', typeof (repostasObtidas));
-
-  return repostasObtidas;
-
+  return obterDadosURL<IReceita[]>('https://gist.githubusercontent.com/antonio-evaldo/002ad55e1cf01ef3fc6ee4feb9152964/raw/bf463b47860043da3b3604ca60cffc3ad1ba9865/receitas.json');
 }
 
 async function obterDadosURL<T>(url: string) {
